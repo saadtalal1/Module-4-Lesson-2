4a95225b91982fc59d233a223ea67de28d89e365
saadtalal1 <saadtalal0112@gmail.com>
2026-05-06 15:31:18 +0400

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,13 @@
+tup1=(4,3,2,2,-1,18)
+tup2=(2,4,8,8,3,2,9)
+result1=1
+for i in tup1:
+    result1*=i
+print("The product of all the numbers in tuple 1 is: ",result1)
+result2=1
+for i in tup2:
+    result2*=i
+print("The product of all the numbers in tuple 2 is: ",result2)
+
+
+
